bd6f97c07d608242b94fe54be3e0d5965f1cd8dc
Lonnie Arellano <lonnie.j.arellano@gmail.com>
2023-02-06 11:44:01 -0600

    added pictures from external api

--- js/index.js
+++ js/index.js
@@ -22,17 +22,20 @@ searchButton.addEventListener(`click`,async ()=>{
 
     let filteredMovies=[];
     let allMovies=await getMovies()
-    for(let movie of allMovies){
+    for(let movie of allMovies) {
         if ((search.value).toLowerCase().startsWith((movie.title).toLowerCase())) {
             filteredMovies.push(movie);
             console.log(filteredMovies[0])
         }
-        for(let i = 0; i < filteredMovies.length; i++){
-            // console.log(movies[i])
+        for (let i = 0; i < filteredMovies.length; i++) {
+            console.log(filteredMovies[i].title)
+            tmdbName(filteredMovies[i].title, (posterUrl) => {
+
             let htmlContent = document.querySelector(`#searchedMovie`)
             htmlContent.innerHTML = `
          <div id="searchCard">
         <h3>${filteredMovies[i].title}</h3>
+        <img src="${posterUrl}" alt="yp" class="search-posters" >
         <ul>
         <li>${filteredMovies[i].year}</li>
         <li>${filteredMovies[i].director}</li>
@@ -42,7 +45,8 @@ searchButton.addEventListener(`click`,async ()=>{
         <li>${filteredMovies[i].actors}</li>
         </ul>
          </div>`
-        }
+        })
+    }
     }
 });
 
@@ -56,41 +60,77 @@ searchButton.addEventListener(`click`,async ()=>{
         }
     }
 
+    // const render = () =>{
+    //     getMovies().then(
+    //         (movies) => {
+    //             let html = ``
+    //             for(let i = 0; i < movies.length; i++){
+    //                 console.log(movies[i].title)
+    //                 let htmlContent = document.querySelector(`#contents`)
+    //                 html += `
+    //     <div class="card">
+    //
+    //     <h3>${movies[i].title}</h3>
+    //     <img src="" alt="almost here">
+    //     <ul>
+    //     <li>${movies[i].year}</li>
+    //     <li>${movies[i].director}</li>
+    //     <li>${movies[i].rating}</li>
+    //     <li>${movies[i].runtime}</li>
+    //     <li>${movies[i].genre}</li>
+    //     <li>${movies[i].actors}</li>
+    //
+    //     </ul>
+    //     </div>`
+    //
+    //
+    //
+    //                 // htmlContent.style.backgroundImage = `url(${})`
+    //                 htmlContent.innerHTML = html
+    //             }
+    //         }
+    //     );
+    // }
 
-    const render = () =>{
-        getMovies().then(
-            (movies) => {
-                let html = ``
-                for(let i = 0; i < movies.length; i++){
-                    // console.log(movies[i])
-                    let htmlContent = document.querySelector(`#contents`)
+    const
+        render = () => {
+        getMovies().then((movies) => {
+            let html = "";
+            for (let i = 0; i < movies.length; i++) {
+                tmdbName(movies[i].title, (posterUrl) => {
+                    let htmlContent = document.querySelector(`#contents`);
                     html += `
-
         <div class="card">
-        
-        <h3>${movies[i].title}</h3>
-        <ul>
-        <li>${movies[i].year}</li>
-        <li>${movies[i].director}</li>
-        <li>${movies[i].rating}</li>
-        <li>${movies[i].runtime}</li>
-        <li>${movies[i].genre}</li>
-        <li>${movies[i].actors}</li>
-        </ul>
-        </div>`
-                    htmlContent.innerHTML = html
-                }
+          <h3>${movies[i].title}</h3>
+          <img src="${posterUrl}" alt="almost here" class="posters">
+          <ul>
+            <li>${movies[i].year}</li>
+            <li>${movies[i].director}</li>
+            <li>${movies[i].rating}</li>
+            <li>${movies[i].runtime}</li>
+            <li>${movies[i].genre}</li>
+            <li>${movies[i].actors}</li>
+          </ul>
+        </div>`;
+                    htmlContent.innerHTML = html;
+                });
             }
-        );
-    }
-    fetch(`https://api.themoviedb.org/3/movie/550?api_key=${keys.tmdb}`)
-        .then(response => response.json())
-        .then(data => {
-            console.log(data);
-        })
-        .catch(error => {
-            console.error('Error:', error);
         });
+    };
+    function tmdbName(input, callback) {
+        fetch(
+            `https://api.themoviedb.org/3/search/movie?api_key=${keys.tmdb}&query=${encodeURIComponent(input)}&page=1`)
+            .then((response) => response.json())
+            .then((data) => {
+                let posterPath = data.results[0].poster_path;
+                let posterUrl = `https://image.tmdb.org/t/p/w500/${posterPath}`;
+                callback(posterUrl);
+            })
+            .catch((error) => {
+                console.error("Error:", error);
+            });
+    }
+
 
     addButton.addEventListener("click", (e) => {
         e.preventDefault()
